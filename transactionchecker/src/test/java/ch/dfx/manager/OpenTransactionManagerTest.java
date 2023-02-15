3ff27215e12132207665933103e7a324ec25c8d6
bernd <bernd@dfx.swiss>
2023-02-15 08:29:41 +0100

    added new tests

--- transactionchecker/src/test/java/ch/dfx/manager/OpenTransactionManagerTest.java
+++ transactionchecker/src/test/java/ch/dfx/manager/OpenTransactionManagerTest.java
@@ -1,11 +1,15 @@
 package ch.dfx.manager;
 
 import static ch.dfx.transactionserver.database.DatabaseUtils.TOKEN_NETWORK_SCHEMA;
+import static ch.dfx.transactionserver.database.DatabaseUtils.TOKEN_STAKING_SCHEMA;
+import static ch.dfx.transactionserver.database.DatabaseUtils.TOKEN_YIELDMACHINE_SCHEMA;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 import java.io.File;
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -21,7 +25,6 @@ import ch.dfx.api.data.transaction.OpenTransactionInvalidatedDTO;
 import ch.dfx.api.data.transaction.OpenTransactionVerifiedDTO;
 import ch.dfx.common.enumeration.NetworkEnum;
 import ch.dfx.defichain.provider.DefiDataProvider;
-import ch.dfx.transactionserver.database.DatabaseUtils;
 import ch.dfx.transactionserver.database.H2DBManager;
 
 /**
@@ -76,10 +79,9 @@ public class OpenTransactionManagerTest {
    */
   @Before
   public void before() {
-    String deleteSql = "api_duplicate_check";
-    deleteSql = DatabaseUtils.replaceSchema(NetworkEnum.TESTNET, deleteSql);
-
-    TestUtils.sqlDelete(TOKEN_NETWORK_SCHEMA, deleteSql);
+    TestUtils.sqlDelete(TOKEN_NETWORK_SCHEMA, "api_duplicate_check");
+    TestUtils.sqlDelete(TOKEN_STAKING_SCHEMA, "staking_withdrawal_reserved");
+    TestUtils.sqlDelete(TOKEN_YIELDMACHINE_SCHEMA, "staking_withdrawal_reserved");
   }
 
   @Test
@@ -113,8 +115,8 @@ public class OpenTransactionManagerTest {
   }
 
   @Test
-  public void validTransactionTest() {
-    LOGGER.debug("validTransactionTest()");
+  public void validStakingDFIWithdrawalTransactionTest() {
+    LOGGER.debug("validStakingDFIWithdrawalTransactionTest()");
 
     try {
       TestUtils.setJSONTransactionAndWithdrawalFile(
@@ -136,6 +138,166 @@ public class OpenTransactionManagerTest {
 
       assertEquals("JSON Response List Size", 1, jsonResponseList.size());
       assertEquals("JSON Response", verifiedDTO.toString(), jsonResponseList.get(0));
+
+      // ...
+      List<Map<String, Object>> apiDuplicateCheckDataList =
+          TestUtils.sqlSelect(TOKEN_NETWORK_SCHEMA, "api_duplicate_check", null);
+
+      assertEquals("API Duplicate Check Size", 1, apiDuplicateCheckDataList.size());
+
+      Map<String, Object> apiDuplicateCheckDataMap = apiDuplicateCheckDataList.get(0);
+
+      assertEquals("API Duplicate Check Withdrawal Id", 2l, apiDuplicateCheckDataMap.get("WITHDRAWAL_ID"));
+      assertEquals(
+          "API Duplicate Check Transaction Id", "440a351c87bb030c774c4b822504bc954188dde6321b05bb5b52af357098f688",
+          apiDuplicateCheckDataMap.get("TRANSACTION_ID"));
+
+      // ...
+      List<Map<String, Object>> stakingReservedDataList =
+          TestUtils.sqlSelect(TOKEN_STAKING_SCHEMA, "staking_withdrawal_reserved", null);
+
+      List<Map<String, Object>> yieldmachineReservedDataList =
+          TestUtils.sqlSelect(TOKEN_YIELDMACHINE_SCHEMA, "staking_withdrawal_reserved", null);
+
+      assertEquals("Staking Withdrawal Reserved Size", 1, stakingReservedDataList.size());
+      assertEquals("Yieldmachine Withdrawal Reserved Size", 0, yieldmachineReservedDataList.size());
+
+      Map<String, Object> stakingReservedDataMap = stakingReservedDataList.get(0);
+
+      assertEquals("Reserved Token Number", 0, stakingReservedDataMap.get("TOKEN_NUMBER"));
+      assertEquals("Reserved Withdrawal Id", 2l, stakingReservedDataMap.get("WITHDRAWAL_ID"));
+      assertEquals(
+          "Reserved Transaction Id", "440a351c87bb030c774c4b822504bc954188dde6321b05bb5b52af357098f688", stakingReservedDataMap.get("TRANSACTION_ID"));
+      assertEquals("Reserved Customer Address", "tf1qwufuhfrkyhprnsylxcj566lax7ntnza305sna7", stakingReservedDataMap.get("CUSTOMER_ADDRESS"));
+      assertEquals("Reserved Amount", new BigDecimal("5.00000000"), stakingReservedDataMap.get("VOUT"));
+    } catch (Exception e) {
+      fail("no exception expected: " + e.getMessage());
+    }
+  }
+
+  @Test
+  public void validYieldmachineDFIWithdrawalTransactionTest() {
+    LOGGER.debug("validYieldmachineDFIWithdrawalTransactionTest()");
+
+    try {
+      TestUtils.setJSONTransactionAndWithdrawalFile(
+          "json/withdrawal/good/02-transaction.json",
+          "json/withdrawal/good/02-withdrawal.json");
+      TestUtils.setJSONChainTransactionFile("json/withdrawal/02-chaindata-transaction.json");
+      TestUtils.setJSONChainCustomTransactionFile("json/withdrawal/02-chaindata-custom-transaction.json");
+
+      // ...
+      OpenTransactionVerifiedDTO verifiedDTO = new OpenTransactionVerifiedDTO();
+      verifiedDTO.setSignature("dataValidTest-signature");
+
+      TestUtils.setDataProviderMock(verifiedDTO.getSignature());
+
+      // ...
+      transactionManager.execute();
+
+      // ...
+      List<String> jsonResponseList = TestUtils.apiTransactionRequestHandler.getJSONResponseList();
+
+      assertEquals("JSON Response List Size", 1, jsonResponseList.size());
+      assertEquals("JSON Response", verifiedDTO.toString(), jsonResponseList.get(0));
+
+      // ...
+      List<Map<String, Object>> apiDuplicateCheckDataList =
+          TestUtils.sqlSelect(TOKEN_NETWORK_SCHEMA, "api_duplicate_check", null);
+
+      assertEquals("API Duplicate Check Size", 1, apiDuplicateCheckDataList.size());
+
+      Map<String, Object> apiDuplicateCheckDataMap = apiDuplicateCheckDataList.get(0);
+
+      assertEquals("API Duplicate Check Withdrawal Id", 4297l, apiDuplicateCheckDataMap.get("WITHDRAWAL_ID"));
+      assertEquals(
+          "API Duplicate Check Transaction Id", "0627d1b0763daa844af6bb0ce2a3e6b7b76ae7a0a85284421a27509bf48e070f",
+          apiDuplicateCheckDataMap.get("TRANSACTION_ID"));
+
+      // ...
+      List<Map<String, Object>> stakingReservedAfterDataList =
+          TestUtils.sqlSelect(
+              TOKEN_STAKING_SCHEMA, "staking_withdrawal_reserved", null);
+
+      List<Map<String, Object>> yieldmachineReservedAfterDataList =
+          TestUtils.sqlSelect(
+              TOKEN_YIELDMACHINE_SCHEMA, "staking_withdrawal_reserved", null);
+
+      assertEquals("Staking Withdrawal Reserved Size", 0, stakingReservedAfterDataList.size());
+      assertEquals("Yieldmachine Withdrawal Reserved Size", 1, yieldmachineReservedAfterDataList.size());
+
+      Map<String, Object> yieldmachineReservedDataMap = yieldmachineReservedAfterDataList.get(0);
+
+      assertEquals("Reserved Token Number", 0, yieldmachineReservedDataMap.get("TOKEN_NUMBER"));
+      assertEquals("Reserved Withdrawal Id", 4297l, yieldmachineReservedDataMap.get("WITHDRAWAL_ID"));
+      assertEquals(
+          "Reserved Transaction Id", "0627d1b0763daa844af6bb0ce2a3e6b7b76ae7a0a85284421a27509bf48e070f", yieldmachineReservedDataMap.get("TRANSACTION_ID"));
+      assertEquals("Reserved Customer Address", "df1qek5mfpxxdz922rd0zpvkue4qtxj6r0qhdt6v7s", yieldmachineReservedDataMap.get("CUSTOMER_ADDRESS"));
+      assertEquals("Reserved Amount", new BigDecimal("800.00000000"), yieldmachineReservedDataMap.get("VOUT"));
+    } catch (Exception e) {
+      fail("no exception expected: " + e.getMessage());
+    }
+  }
+
+  @Test
+  public void validYieldmachineDUSDWithdrawalTransactionTest() {
+    LOGGER.debug("validYieldmachineDUSDWithdrawalTransactionTest()");
+
+    try {
+      TestUtils.setJSONTransactionAndWithdrawalFile(
+          "json/withdrawal/good/03-transaction.json",
+          "json/withdrawal/good/03-withdrawal.json");
+      TestUtils.setJSONChainTransactionFile("json/withdrawal/03-chaindata-transaction.json");
+      TestUtils.setJSONChainCustomTransactionFile("json/withdrawal/03-chaindata-custom-transaction.json");
+
+      // ...
+      OpenTransactionVerifiedDTO verifiedDTO = new OpenTransactionVerifiedDTO();
+      verifiedDTO.setSignature("dataValidTest-signature");
+
+      TestUtils.setDataProviderMock(verifiedDTO.getSignature());
+
+      // ...
+      transactionManager.execute();
+
+      // ...
+      List<String> jsonResponseList = TestUtils.apiTransactionRequestHandler.getJSONResponseList();
+
+      assertEquals("JSON Response List Size", 1, jsonResponseList.size());
+      assertEquals("JSON Response", verifiedDTO.toString(), jsonResponseList.get(0));
+
+      // ...
+      List<Map<String, Object>> apiDuplicateCheckDataList =
+          TestUtils.sqlSelect(TOKEN_NETWORK_SCHEMA, "api_duplicate_check", null);
+
+      assertEquals("API Duplicate Check Size", 1, apiDuplicateCheckDataList.size());
+
+      Map<String, Object> apiDuplicateCheckDataMap = apiDuplicateCheckDataList.get(0);
+
+      assertEquals("API Duplicate Check Withdrawal Id", 4298l, apiDuplicateCheckDataMap.get("WITHDRAWAL_ID"));
+      assertEquals(
+          "API Duplicate Check Transaction Id", "56fc2de46c70274527a657e986e767fdff281fdaa826d96ccd35f78813ad5c4b",
+          apiDuplicateCheckDataMap.get("TRANSACTION_ID"));
+
+      // ...
+      List<Map<String, Object>> stakingReservedAfterDataList =
+          TestUtils.sqlSelect(
+              TOKEN_STAKING_SCHEMA, "staking_withdrawal_reserved", null);
+
+      List<Map<String, Object>> yieldmachineReservedAfterDataList =
+          TestUtils.sqlSelect(
+              TOKEN_YIELDMACHINE_SCHEMA, "staking_withdrawal_reserved", null);
+
+      assertEquals("Staking Withdrawal Reserved Size", 0, stakingReservedAfterDataList.size());
+      assertEquals("Yieldmachine Withdrawal Reserved Size", 1, yieldmachineReservedAfterDataList.size());
+
+      Map<String, Object> yieldmachineReservedDataMap = yieldmachineReservedAfterDataList.get(0);
+
+      assertEquals("Reserved Token Number", 11, yieldmachineReservedDataMap.get("TOKEN_NUMBER"));
+      assertEquals("Reserved Withdrawal Id", 4298l, yieldmachineReservedDataMap.get("WITHDRAWAL_ID"));
+      assertEquals(
+          "Reserved Transaction Id", "56fc2de46c70274527a657e986e767fdff281fdaa826d96ccd35f78813ad5c4b", yieldmachineReservedDataMap.get("TRANSACTION_ID"));
+      assertEquals("Reserved Customer Address", "df1qgz2xyzqwnsn5979syu6ng9wxlc4c2ac98m377f", yieldmachineReservedDataMap.get("CUSTOMER_ADDRESS"));
+      assertEquals("Reserved Amount", new BigDecimal("500.00000000"), yieldmachineReservedDataMap.get("VOUT"));
     } catch (Exception e) {
       fail("no exception expected: " + e.getMessage());
     }
--- transactionchecker/src/test/resources/sql/openTransactionManagerTest.sql
+++ transactionchecker/src/test/resources/sql/openTransactionManagerTest.sql
@@ -17,6 +17,22 @@ VALUES (3, 'tf1qwufuhfrkyhprnsylxcj566lax7ntnza305sna7');
 INSERT INTO public.address (number, address)
 VALUES (4, 'df1qgz2xyzqwnsn5979syu6ng9wxlc4c2ac98m377f');
 
+-- Yieldmachine: Liquidity
+INSERT INTO public.address (number, address)
+VALUES (11, 'df1qasdh79grpyd2wltpamznqp2xdhq6kmkevafxgq');
+
+-- Yieldmachine: Deposit / Customer
+INSERT INTO public.address (number, address)
+VALUES (12, 'df1q5r9u6px6j04a97y56qfkn2wrjct3yteynlegrg');
+
+INSERT INTO public.address (number, address)
+VALUES (13, 'df1qek5mfpxxdz922rd0zpvkue4qtxj6r0qhdt6v7s');
+
+-- Yieldmachine: Deposit / Customer
+INSERT INTO public.address (number, address)
+VALUES (14, 'df1qa06vpd0ts58223dchtet6e089m4czf4k604drn');
+
+
 -- ============================================================================
 -- SCHEMA: TESTNET
 -- ============================================================================
@@ -40,8 +56,17 @@ VALUES (1, 2, 'tf1qqkzs9mrlwq0qy0dw74wpvyuup0gwhak7jppxcv');
 INSERT INTO testnet_staking.staking_address (token_number, liquidity_address_number, start_block_number, start_transaction_number)
 VALUES (0, 1, 0, 0);
 
+INSERT INTO testnet_yieldmachine.staking_address (token_number, liquidity_address_number, start_block_number, start_transaction_number)
+VALUES (0, 11, 0, 0);
+
 -- =======================
 -- TESTNET_STAKING.STAKING
 -- =======================
 INSERT INTO testnet_staking.staking (token_number, liquidity_address_number, deposit_address_number, customer_address_number, last_in_block_number, vin, last_out_block_number, vout)
 VALUES (0, 1, 2, 3, 0, 500.00000000, 0, 100.00000000);
+
+INSERT INTO testnet_yieldmachine.staking (token_number, liquidity_address_number, deposit_address_number, customer_address_number, last_in_block_number, vin, last_out_block_number, vout)
+VALUES (0, 11, 12, 13, 0, 1000.00000000, 0, 100.00000000);
+
+INSERT INTO testnet_yieldmachine.staking (token_number, liquidity_address_number, deposit_address_number, customer_address_number, last_in_block_number, vin, last_out_block_number, vout)
+VALUES (11, 11, 14, 4, 0, 500.00000000, 0, 0.00000000);
